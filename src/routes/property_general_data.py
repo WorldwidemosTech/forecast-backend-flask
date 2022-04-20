f59c0ae54356d9d72f0f27399d1b7c84cd267124
dlopezvsr <diego_lo.vi@icloud.com>
2022-04-20 18:57:03 -0500

    Add id to schema validator, logic of general data endpoint

--- src/routes/property_general_data.py
+++ src/routes/property_general_data.py
@@ -1,6 +1,11 @@
+import re
 from flask import Blueprint, request
-
+from src.config.logger import logger
+import json
+from src.utilities.respond import success
+from src.utilities.exceptions.exceptionfactory import ExceptionFactory
 from src.utilities.schemahandler import SchemaHandler
+from src.db_config import user, property_general_data
 
 property_bp = Blueprint(name="property", import_name=__name__)
 schema_handler = SchemaHandler()
@@ -9,39 +14,85 @@ schema_handler = SchemaHandler()
 @property_bp.route('/property/<string:property_id>', methods=['GET'])
 def get_property(user_id: str, property_id: str):
     """Gets an individual property general data."""
-    print("UserId: ", user_id)
-    print("PropertyId: ", property_id)
-    return {"success": True, "message": "property_data"}
+
+    logger.info(f"UserId: {user_id}")
+    logger.info(f"PropertyId: {property_id}")
+
+    response = property_general_data.find_one({'property_id': property_id,
+                                            'user_id': user_id})
+    logger.info(f"Data: {response}")
+    if response == None:
+        raise ExceptionFactory("Information not found").invalid_dict_parameter_value()
+    response["_id"] = str(response["_id"])
+    return {"success": True, "message": "information_data", "body": json.loads(json.dumps(response))}
 
 
 @property_bp.route('/property', methods=['GET'])
 def get_property_list(user_id: str):
     """Gets a list of properties general data associated to user."""
-    print("UserId: ", user_id)
-    return {"success": True, "message": "property_list"}
+
+    logger.info(f"UserId: {user_id}")
+    validator = len(list(user.find({'user_id': user_id})))
+    response = property_general_data.find({'user_id': user_id})
+    logger.info(f"Data: {response}")
+    
+    if validator == 0:
+        raise ExceptionFactory("").database_operation_failed()
+    list_items = []
+    
+    for i in response:
+        i["_id"] = str(i["_id"])
+        list_items.append(i)
+        logger.info(i)
+    return {"success": True, "message": "List of properties", "body": json.loads(json.dumps(list_items))}
+
 
 
 @property_bp.route('/property', methods=['POST'])
 def create_property(user_id: str):
     """Creates a property general data."""
-    print("UserId: ", user_id)
-    data = request.json
-    schema_handler.validate_property_data(request.json)
-    return {"success": True, "message": "property_data"}
+
+    logger.info(f"UserId: {user_id}")
+    response = user.find_one({'user_id': user_id})
+    logger.info(f"Response: {response}")
+    if response == None:
+        raise ExceptionFactory("").database_operation_failed()
+    else:
+        data = request.json
+        data["user_id"] = user_id
+        schema_handler.validate_property_data(data)
+        property_general_data.insert_one(data)
+        return success("General information created")
 
 
 @property_bp.route('/property/<string:property_id>', methods=['PUT'])
 def update_property(user_id: str, property_id: str):
     """Updates individual property general data."""
-    print("UserId: ", user_id)
-    print("PropertyId: ", property_id)
-    data = request.json
-    return {"success": True, "message": "property_data"}
+   
+    response = property_general_data.find_one({'user_id': user_id, 'property_id': property_id})
+    if response == None:
+        raise ExceptionFactory("").database_operation_failed()
+
+    else:
+        data = request.json
+        schema_handler.validate_property_data(data)
+        response = property_general_data.update_one({'user_id': user_id}, {'$set': data})
+        logger.info(f"Response: {response}")
+        return success("General data updated")
 
 
 @property_bp.route('/property/<string:property_id>', methods=['DELETE'])
 def delete_property(user_id: str, property_id: str):
     """Deletes a property general data."""
-    print("UserId: ", user_id)
-    print("PropertyId: ", property_id)
-    return {"success": True, "message": "property_data"}
+
+    logger.info(f"UserId: {user_id}")
+    logger.info(f"PropertyId: {property_id}")
+    query = {'user_id': user_id, 'property_id': property_id}
+    response = property_general_data.find_one(query)
+
+    if response == None:
+        raise ExceptionFactory("").database_operation_failed()
+    else:
+        property_general_data.delete_one({'user_id': user_id,
+                                            'property_id': property_id})
+        return success("information data deleted")
--- src/routes/property_information.py
+++ src/routes/property_information.py
@@ -14,6 +14,7 @@ schema_handler = SchemaHandler()
 @information_bp.route('/information/<string:property_id>', methods=['GET'])
 def get_information(user_id: str, property_id: str):
     """Gets an individual property information."""
+
     logger.info(f"UserId: {user_id}")
     logger.info(f"PropertyId: {property_id}")
     data = property_information.find_one({'property_id': property_id,
@@ -30,6 +31,7 @@ def get_information(user_id: str, property_id: str):
 @information_bp.route('/information', methods=['POST'])
 def create_information(user_id: str):
     """Creates a property information."""
+
     response = user.find_one({'user_id': user_id})
     logger.info(f"Response: {response}")
     if response == None:
@@ -44,6 +46,7 @@ def create_information(user_id: str):
 @information_bp.route('/information/<string:property_id>', methods=['PUT'])
 def update_information(user_id: str, property_id: str):
     """Updates individual property information."""
+
     logger.info(f"UserId: {user_id}")
     logger.info(f"PropertyId: {property_id}")
     data = request.json
@@ -62,6 +65,7 @@ def update_information(user_id: str, property_id: str):
 @information_bp.route('/information/<string:property_id>', methods=['DELETE'])
 def delete_information(user_id: str, property_id: str):
     """Deletes a property information."""
+    
     logger.info(f"UserId: {user_id}")
     logger.info(f"PropertyId: {property_id}")
     query = {'user_id': user_id, 'property_id': property_id}
