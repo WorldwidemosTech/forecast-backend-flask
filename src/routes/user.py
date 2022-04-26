b7571a46e7163a998921208c7a2a6e36b8c8ad3c
dlopezvsr <diego_lo.vi@icloud.com>
2022-04-25 20:18:18 -0500

    endpoint user

--- src/routes/user.py
+++ src/routes/user.py
@@ -2,6 +2,10 @@ from flask import Blueprint, request
 
 from src.utilities.schemahandler import SchemaHandler
 from src.utilities.respond import success
+from src.db_config import user
+from src.utilities.exceptions.exceptionfactory import ExceptionFactory
+import json
+from src.config.logger import logger
 
 user_bp = Blueprint(name="user", import_name=__name__)
 schema_handler = SchemaHandler()
@@ -10,23 +14,44 @@ schema_handler = SchemaHandler()
 @user_bp.route('/user/<string:user_id>', methods=['GET'])
 def get_user(user_id: str):
     """Gets an individual user data."""
-    return success({"data": "user_data"})
+    response = user.find_one({'user_id': user_id})
+    response['_id'] = str(response["_id"])
+    if response == None:
+        raise ExceptionFactory("Information not found").invalid_dict_parameter_value()
+    return {"success": True, "message": "information_data", "body": json.loads(json.dumps(response))}
 
 
 @user_bp.route('/user', methods=['POST'])
 def create_user():
     """Creates a user."""
-    schema_handler.validate_user(request.json)
-    return success()
+    data = request.json
+    schema_handler.validate_user(data)
+    response = user.insert_one(data)
+
+    if response.inserted_id == None:
+        raise ExceptionFactory("User not created").invalid_dict_parameter_value()
+    logger.info(str(response.inserted_id))
+    return success(f"User created successfully with id: {str(response.inserted_id)}")
 
 
 @user_bp.route('/user/<string:user_id>', methods=['PUT'])
 def update_user(user_id: str):
     """Updates individual user data."""
-    return success()
+    data = request.json
+    schema_handler.validate_user(data)
+    response = user.update_one({'user_id': user_id}, {'$set': data})
+    if response.modified_count < 1:
+        raise ExceptionFactory("").database_operation_failed()
+    return success("User updated successfully")
 
 
 @user_bp.route('/user/<string:user_id>', methods=['DELETE'])
 def delete_user(user_id: str):
     """Deletes a user data."""
-    return success()
+    logger.info(f"UserId: {user_id}")
+    
+    response = user.delete_one({'user_id': user_id})
+    if response.deleted_count < 1:
+        raise ExceptionFactory("").database_operation_failed()
+        
+    return success("information data deleted")
