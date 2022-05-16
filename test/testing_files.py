29776769528bc216a3572e359ea5156954ae97f5
dlopezvsr <diego_lo.vi@icloud.com>
2022-05-16 17:54:59 -0500

    Formulas and more formulas

--- test/testing_files.py
+++ test/testing_files.py
@@ -1,10 +1,10 @@
-gross_rent_potential = 11
-grp_time_series = [gross_rent_potential]
+occupancy_goals_precentage = 93
+occupancy_goals_list = [occupancy_goals_precentage]
+for month in range(11):
+    if occupancy_goals_precentage < 94:
+        occupancy_goals_precentage += (occupancy_goals_precentage * .01)
+    else:
+        occupancy_goals_precentage = 95
+    occupancy_goals_list.append(round(occupancy_goals_precentage,2))
 
-for value in range(11):
-    grp_time_series.append(gross_rent_potential + grp_time_series[value])
-
-
-montlhy_moveouts = round(300 * 14.5/12)
-
-print(float(montlhy_moveouts*12))
+print(occupancy_goals_list)
