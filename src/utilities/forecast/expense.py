46163fa94947943f27453a0fd8ea8343ed2905bc
dlopezvsr <diego_lo.vi@icloud.com>
2022-07-06 20:04:12 -0500

    last change

--- src/utilities/forecast/expense.py
+++ src/utilities/forecast/expense.py
@@ -59,14 +59,34 @@ class Expense(Property):
         
         for employees in expense_inputs["employee_expense"]:
             employee_info["employees_salary"].append(employees["hourly_rate"] * employees["monthly_hours"] * employees["number_eployees"])
-            employee_info["total_employees"].append()
+            employee_info["total_employees"].append(employees["number_eployees"])
 
-
+        self.expense_schema["employees_salary_expense"] = sum(employee_info["employees_salary"])
+        self.expense_schema["total_number_employees"] = sum(employee_info["total_employees"])
     
+    def employee_insurance(self):
+        expense_inputs = Property.expense_input_info(self)
+        self.expense_schema["health_insurance"] = expense_inputs["employee_insurance_percentage"] * self.expense_schema["employees_salary_expense"]
+
+    def payroll_fee(self):
+        expense_inputs = Property.expense_input_info(self)
+        self.expense_schema["payroll_fees"] = expense_inputs["payroll_fee_per_employee"] * self.expense_schema["total_number_employees"]
+
+    def payroll_tax(self):
+        expense_inputs = Property.expense_input_info(self)
+        self.expense_schema["payroll_tax"] = expense_inputs["payroll_tax_percentage"] * self.expense_schema["employees_salary_expense"]
+
+    def worker_compensation(self):
+        expense_inputs = Property.expense_input_info(self)
+        self.expense_schema["worker_compensation"] = expense_inputs["workers_compensation_percentage"] * (self.expense_schema["health_insurance"]+self.expense_schema["employees_salary_expense"])
+       
+    def property_insurance(self):
+        expense_inputs = Property.expense_input_info(self)
+        units = sum(Property.units_info(self)["total_units"])
+        self.expense_schema["property_insurance"] = round(expense_inputs["insurance_per_unit"] * units)/12
 
 
 
-        
 
 def main():
     expense = Expense("dlopezvsr", "6223cf8c40b07aaf6c4f36b1")
